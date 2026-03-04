2caebbeb485437bf9e03b2edcdd3c379c09ba082
Yash Pouranik <yashpouranik124@gmail.com>
2026-03-04 19:02:59 +0530

    Increase request limit for getAllReleases route

--- backend/routes/releases.js
+++ backend/routes/releases.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authorization = require('../middleware/authMiddleware');
 const { getAllReleases, createRelease } = require('../controllers/release.controller');
+const RateLimit = require('express-rate-limit');
+
+const getAllReleasesLimiter = RateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000, 
+});
 
 // GET ALL RELEASES (Public)
-router.get('/', getAllReleases);
+router.get('/', getAllReleasesLimiter, getAllReleases);
 
 // CREATE RELEASE (Admin Only)
 router.post('/', authorization, createRelease);
